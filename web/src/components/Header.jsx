72f50e75ea2fb52b96cec66099e5e47ec56f2c14
Christian Gaayuoni <gayuoni@proton.me>
2023-11-09 10:54:31 +0100

    Add basic features with no function

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,3 +1,10 @@
+import "./Header.css";
 export function Header() {
-  return <>Header component</>;
+  return (
+    <header className="header-container">
+      <nav className="nav-bar">
+        <p> Home </p> <p> Offers </p> <p> Request </p>
+      </nav>
+    </header>
+  );
 }
