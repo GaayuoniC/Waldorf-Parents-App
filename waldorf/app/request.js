a07cd7e2c9ab1b1d2fe6e5b5ddcc734687dafe17
Christian Gaayuoni <gayuoni@proton.me>
2023-11-13 14:53:58 +0100

    initial commit

--- waldorf/app/request.js
+++ waldorf/app/request.js
@@ -1,5 +1,11 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Requests() {
-  return <Text>Request for help</Text>;
+  return (
+    <View>
+      <Text>Request for help</Text>
+      <Link href="/"> Home</Link>
+    </View>
+  );
 }
