0f42a28aa9850729e1d907eb55041f5c14162bf5
Christian Gaayuoni <gayuoni@proton.me>
2023-11-13 12:26:35 +0100

    add user form and usestate and handle functions

--- web/src/pages/RegistrationPage.jsx
+++ web/src/pages/RegistrationPage.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
 export function RegistrationPage() {
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  console.log("User details", userData);
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setUserData((prev) => ({ ...prev, [name]: value }));
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  console.log("User details", userData);
+
   return (
-    <>
+    <div>
       <h2> Registration Page </h2>
-    </>
+      <form>
+        <label htmlFor="registration">
+          Enter username:
+          <input
+            type="text"
+            name="username"
+            value={userData.username}
+            onChange={handleChange}
+          />{" "}
+        </label>
+      </form>
+    </div>
   );
 }
