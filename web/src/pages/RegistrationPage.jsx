a92189eabd7b292b95ad59edac010f5f0046d575
Christian Gaayuoni <gayuoni@proton.me>
2023-11-13 13:00:55 +0100

    add registration form minus picture upload feature

--- web/src/pages/RegistrationPage.jsx
+++ web/src/pages/RegistrationPage.jsx
@@ -1,11 +1,28 @@
 import { useState } from "react";
+import "../pages/RegistrationPage.css";
 export function RegistrationPage() {
   const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
     username: "",
     email: "",
     password: "",
   });
-  console.log("User details", userData);
+  //console.log("User details", userData);
+
+  const [selectedFoto, setSelectedFoto] = useState(null);
+
+  function handleFotoChange(event) {
+    const file = event.target.files[0];
+    selectedFoto(file);
+  }
+  //   function handleUploadFoto(){
+  //     if(selectedFoto){
+
+  //     }
+  //   }
+  //Need to come back and figure out how to link it to API!
+
   function handleChange(e) {
     const { name, value } = e.target;
     setUserData((prev) => ({ ...prev, [name]: value }));
@@ -18,16 +35,59 @@ export function RegistrationPage() {
   return (
     <div>
       <h2> Registration Page </h2>
+      <label htmlFor="firstName" className="firstName">
+        Enter first name:
+        <input
+          type="text"
+          name="firstName"
+          value={userData.firstName}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label htmlFor="lastName" className="lastName">
+        Enter last name:
+        <input
+          type="text"
+          name="lastName"
+          value={userData.lastName}
+          onChange={handleChange}
+        />
+      </label>
       <form>
-        <label htmlFor="registration">
+        <label htmlFor="username">
           Enter username:
           <input
             type="text"
             name="username"
             value={userData.username}
             onChange={handleChange}
-          />{" "}
+          />
         </label>
+        <br />
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={userData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={userData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit" onSubmit={handleSubmit}>
+          Register
+        </button>
       </form>
     </div>
   );
