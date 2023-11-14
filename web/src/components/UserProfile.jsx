86932d15deebca21f52f3ad5ff607c09bffc76dd
Christian Gaayuoni <gayuoni@proton.me>
2023-11-14 12:25:15 +0100

    login form with functions

--- web/src/components/UserProfile.jsx
+++ web/src/components/UserProfile.jsx
@@ -2,14 +2,14 @@ export function UserProfile({
   profile = {
     name: "Rob Green",
     email: "dee@hotmail",
-    avatar: "https://pokeapi.co/api/v2/pokemon/1",
+    avatar: "/src/images/userprofile.jpg",
   },
 }) {
   const { name, email, avatar } = profile;
 
   return (
     <div className="user-profile">
-      <img src={avatar} alt={name} width={50} height={20} />
+      <img src={avatar} alt={name} width={70} height={70} />
       <h4>{name}</h4>
       <p>Email: {email}</p>
     </div>
