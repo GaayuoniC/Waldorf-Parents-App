e6d3eff1d2433e9406788e4570a36058de90cbfe
Christian Gaayuoni <gayuoni@proton.me>
2023-11-14 14:52:43 +0100

    Nav link corrected

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,23 +1,23 @@
 import "./Header.css";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 export function Header() {
   return (
     <header className="header-container">
       <nav className="nav-bar">
         <NavLink
           to="/"
-          className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "active" : ""
-          }
+          // className={({ isActive, isPending }) =>
+          //   isPending ? "pending" : isActive ? "active" : ""
+          // }
         >
           <p> Home </p>
         </NavLink>
-        <Link to="/offers" className="links">
+        <NavLink to="/offers" className="links">
           <p> Offers </p>
-        </Link>
-        <Link to="/requests" className="links">
+        </NavLink>
+        <NavLink to="/requests" className="links">
           <p> Requests </p>
-        </Link>
+        </NavLink>
       </nav>
     </header>
   );
