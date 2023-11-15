25395b8643b9dbfc4d724a71977ecf4d1bf96cc9
Christian Gaayuoni <gayuoni@proton.me>
2023-11-15 10:56:23 +0100

    initial commit

---
+++ web/src/components/OfferForm.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export function OfferForm() {
+  const [offerTitle, setOfferTitle] = useState("");
+  const [offerDescription, setOfferDescription] = useState("");
+
+  function handleSubmitOfferForm() {
+    event.preventDefault();
+    //validate and then submit the form with this if statement
+    if (offerTitle && offerDescription) {
+      onSubmit({ offerTitle, offerDescription });
+      setOfferTitle("");
+      setOfferDescription("");
+    } else {
+      alert("Please fill in all fields");
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmitOfferForm}>
+      <label>
+        Title:
+        <input
+          type="text"
+          value={offerTitle}
+          onChange={(e) => setOfferTitle(e.target.value)}
+        ></input>
+      </label>
+      <br />
+      <label>
+        Offer Description:
+        <textarea
+          value={offerDescription}
+          onChange={(e) => setOfferDescription(e.target.value)}
+        ></textarea>
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
