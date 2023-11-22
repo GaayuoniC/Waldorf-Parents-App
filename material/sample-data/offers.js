be5a109f85768a9f7a4707af5c53ce4b748817ab
Ralf Siewert <ralf@actyvy.st>
2023-11-22 15:28:48 +0100

    add offer sample data

---
+++ material/sample-data/offers.js
@@ -0,0 +1,10 @@
+export const offers = [
+  {
+    id: "abcde",
+    parentName: "Christian Gayuoni",
+    startingAddress: "Via Roma 1, 6900 Lugano",
+    date: "2023-12-06",
+    modeOfTransportation: "car", // car, bus, bike, walk
+    direction: "to_school", // to_school, from_school, both
+  },
+];
