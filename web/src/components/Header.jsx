bf103bb2dd9e38428db899ef4d6c378e24b451b4
Christian Gaayuoni <gayuoni@proton.me>
2023-11-24 09:45:13 +0100

    changed header

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -23,7 +23,6 @@ export function Header() {
         </NavLink>
         {isSignedIn && (
           <NavLink to="/userprofile" className="links">
-            {" "}
             Profile
           </NavLink>
         )}
