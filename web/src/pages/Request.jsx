4633ead4e1f9d99db08acce5957b331132fad408
Christian Gaayuoni <gayuoni@proton.me>
2023-11-29 16:34:05 +0100

    add minor changes to request

--- web/src/pages/Request.jsx
+++ web/src/pages/Request.jsx
@@ -31,7 +31,43 @@ export function Requests() {
 
   return (
     <>
-      <h3>Available Requests</h3>
+      <section>
+        <h2>Available Requests</h2>
+        {isloading ? (
+          <h4> Loading offfers...please wait</h4>
+        ) : (
+          <div className="parents-container">
+            {request.map((item) => {
+              return (
+                <ul key={item.id} className="parents-card">
+                  <li className="parent-info">
+                    <span className="parent-info-titles"> Parent name: </span>
+                    {item.parentName} <br />
+                    <span className="parent-info-titles">
+                      Starting address:
+                    </span>
+                    {item.startingAddress}
+                    <br />
+                    <span className="parent-info-titles">
+                      Direction of travel:
+                    </span>
+                    {item.direction}
+                    <br />
+                    <span className="parent-info-titles">
+                      Mode of Transport:
+                    </span>
+                    {item.modeOfTransportation}
+                    <br />
+                    <span className="parent-info-titles"> Date: </span>
+                    {item.dateOfTtransportation}
+                  </li>
+                </ul>
+              );
+            })}
+          </div>
+        )}
+      </section>
+
       <section>
         <span
           onClick={() => {
