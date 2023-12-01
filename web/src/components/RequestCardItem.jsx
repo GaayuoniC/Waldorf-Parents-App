bd81005b134c2e67d06ebaed0e3b1e8281008be7
Christian Gaayuoni <gayuoni@proton.me>
2023-12-01 12:27:57 +0100

    Refactor request page and created a request card component

---
+++ web/src/components/RequestCardItem.jsx
@@ -0,0 +1,28 @@
+export function RequestCardItem({ request }) {
+  return (
+    <li key={request.id} className="parent-info parents-card">
+      <span className="parent-info-titles"> Parent name: </span>
+      {request.parentName} <br />
+      <span className="parent-info-titles">Starting street:</span>
+      {request.startStreet}
+      <br />
+      <span className="parent-info-titles">Starting city:</span>
+      {request.startCity}
+      <br />
+      <span className="parent-info-titles">Zip/Postcode:</span>
+      {request.startZip}
+      <br />
+      <span className="parent-info-titles">Number of kids needing care:</span>
+      {request.numberOfChildren}
+      <br />
+      <span className="parent-info-titles">Direction of travel:</span>
+      {request.direction}
+      <br />
+      <span className="parent-info-titles">Mode of Transport:</span>
+      {request.modeOfTransportation}
+      <br />
+      <span className="parent-info-titles"> Date: </span>
+      {request.dateOfTtransportation}
+    </li>
+  );
+}
